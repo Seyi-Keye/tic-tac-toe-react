1a1225005da8e6060014af5201103addf725ef41
Seyi-Keye <oluwaseyi.aromokeye@andela.com>
2017-05-21 20:40:38 +0100

    Validation class

--- src/api/Validation.js
+++ src/api/Validation.js
@@ -1,35 +1,35 @@
 import localforage from 'localforage';
+let errorArray = [];
 
 export class Validation {
 
   static validationChecker(email, password, firstName, lastName){
-    let errorArray = [];
     const validEmail = Validation.emailChecker(email);
     const validPassword = Validation.lengthChecker(password, 'password');
     const validFirstName = Validation.lengthChecker(firstName, 'firstname');
     const validLastName = Validation.lengthChecker(lastName, 'lastname');
-    // if((validEmail === email) && (validPassword ===password) && (validFirstName === firstName) && (validLastName === lastName)) {
-      if(validEmail && validPassword && validFirstName && validPassword) {
+    if((validEmail === email) && (validPassword ===password) && firstName && lastName && (validFirstName === firstName) && (validLastName === lastName)) {
       return { status: 'Successful', email: validEmail, password: validPassword, firstName: validFirstName, lastName: validLastName, score: 0 };
     }
-    return 'Invalid';
+    return Validation.errors();
   }
 
-  static lengthChecker(field, fieldName){
+  static lengthChecker(field = '', fieldName = ''){
     const trimedfield = field.trim();
-    if(!trimedfield.length) return `${fieldName} cannot be empty`;
-    if(trimedfield.length < 3) return `Invalid ${fieldName}, length of 3 at least`;
+    if(!trimedfield.length) return errorArray.concat(`${fieldName} cannot be empty`);
+    if(trimedfield.length < 3) return errorArray.concat(`Invalid ${fieldName}, length of 3 at least`);
     return trimedfield;
   }
 
   static emailChecker(email){
-    if (!email.length) return 'Email cannot be empty';
-    return /\b[a-zA-Z0-9_]+@[a-z]+\.com\b/.test(email) ? email : 'Invalid email address';
+    if (!email.length) return errorArray.concat('email cannot be empty');
+    return /\b[a-zA-Z0-9_]+@[a-z]+\.com\b/.test(email) ? email : errorArray = errorArray.concat('Invalid email address');
   }
 
-  static errors(errorArray) {
-    errorArray.map((error) => {
-      return error;
-    });
+  static errors() {
+   return errorArray.reduce((acc, error) => {
+       acc = acc + error + '\n';
+       return acc;
+    }, '');
   }
 }
--- src/test/api/Validation.test.js
+++ src/test/api/Validation.test.js
@@ -3,7 +3,6 @@ import { Validation } from '../../api/Validation';
 
 describe('Validation Service Api' , () => {
   it(`returns successful on valid email, password, firstname and lastname`, () => {
-    console.log('======>>', Validation.validationChecker('abc@gmail.com', 'abc', 'abc', 'abc'))
     expect(Validation.validationChecker('abc@gmail.com', 'abc', 'abc', 'abc')).to.deep.equal({
       status: 'Successful',
       email: 'abc@gmail.com',
@@ -15,51 +14,50 @@ describe('Validation Service Api' , () => {
   });
 
   it('returns error on invalid detail', () => {
-    console.log('+++++++>', Validation.validationChecker('abc@gmailcom', 'abc', 'abc', 'abc'))
-    expect(Validation.validationChecker('abc@gmailcom', 'abc')).to.equal('Invalid email address');
+    expect(Validation.validationChecker('abc@gmailcom', 'abc')).to.contain('Invalid email address');
   });
 });
 
 describe('Validation email checker', () => {
   it('returns error on empty email', () => {
-    expect(Validation.emailChecker('')).to.equal('email cannot be empty');
+    expect(Validation.emailChecker('', 'email')).to.contain('email cannot be empty');
   });
 
   it('returns error on invalid email', () => {
-    expect(Validation.emailChecker('abc.com')).to.equal('Invalid email address');
+    expect(Validation.emailChecker('abc.com')).to.contain('Invalid email address');
   });
 
   it('returns email on valid email', () => {
-    expect(Validation.emailChecker('abc@gmail.com')).to.equal('abc@gmail.com');
+    expect(Validation.emailChecker('abc@gmail.com')).to.contain('abc@gmail.com');
   });
 });
 
 describe('Validation password checker', () => {
   it('returns error on empty password', () => {
-    expect(Validation.lengthChecker('', 'password')).to.equal('password cannot be empty');
+    expect(Validation.lengthChecker('', 'password')).to.contain('password cannot be empty');
   });
 
   it('returns error on invalid password', () => {
-    expect(Validation.lengthChecker('a', 'password')).to.equal('Invalid password, length of 3 at least');
+    expect(Validation.lengthChecker('a', 'password')).to.contain('Invalid password, length of 3 at least');
   });
 });
 
 describe('Validation firstname checker', () => {
   it('returns error on empty firstname', () => {
-    expect(Validation.lengthChecker('', 'firstname')).to.equal('firstname cannot be empty');
+    expect(Validation.lengthChecker('', 'firstname')).to.contain('firstname cannot be empty');
   });
 
   it('returns error on invalid firstname', () => {
-    expect(Validation.lengthChecker('a', 'firstname')).to.equal('Invalid firstname, length of 3 at least');
+    expect(Validation.lengthChecker('a', 'firstname')).to.contain('Invalid firstname, length of 3 at least');
   });
 });
 
 describe('Validation lastname checker', () => {
   it('returns error on empty lastname', () => {
-    expect(Validation.lengthChecker('', 'lastname')).to.equal('lastname cannot be empty');
+    expect(Validation.lengthChecker('', 'lastname')).to.contain('lastname cannot be empty');
   });
 
   it('returns error on invalid lastname', () => {
-    expect(Validation.lengthChecker('a', 'lastname')).to.equal('Invalid lastname, length of 3 at least');
+    expect(Validation.lengthChecker('a', 'lastname')).to.contain('Invalid lastname, length of 3 at least');
   });
 });
